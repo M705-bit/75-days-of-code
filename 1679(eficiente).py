c86a6d6e3fe3283edaec9241ece0935cdaaa0118
Marina Hedlund Jardim <84291180+M705-bit@users.noreply.github.com>
2025-08-25 15:15:16 -0300

    Add files via upload

---
+++ 1679(eficiente).py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+class Solution:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        freq = Counter(nums)
+        count = 0
+
+        for num in list(freq.keys()):
+            complement = k - num
+            if complement in freq:
+                if num == complement:
+                    pairs = freq[num] // 2
+                else:
+                    pairs = min(freq[num], freq[complement])
+                count += pairs
+                freq[num] -= pairs
+                freq[complement] -= pairs
+
+        return count
+
+solucao = Solution()
+solucao = Solution()
+k = int(input("entre com um valor para k: "))
+lista = list(map(int, input("entre com uma lista: ").split(",")))
+print(solucao.maxOperations(lista,k))
